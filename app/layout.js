1a9922b58d32b18ead9e86a8fcb52fe98f1d6659
MarlonWelimaluwa <marlonjr787@hotmail.com>
2026-01-07 22:04:00 +0530

    Speed Test Integration

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en" className="dark">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+        {children}
+        <SpeedInsights />
+        </body>
         </html>
     );
 }
